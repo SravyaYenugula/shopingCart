5c1cdd952d27c5bea5054bc656364b362dd7b595
Sravya Yenugula <sravyayenugula@gmail.com>
2023-11-23 18:45:53 +0530

    second commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,56 +1,26 @@
 import { Form, Input, Select, Modal, Table, Button, Tag, List, Card, Typography, Badge, Rate, message, Drawer } from "antd";
 import { useNavigate } from "react-router-dom";
 
-import { Row, Col, Space, Popconfirm,Spin,Menu } from "antd";
 import React, { useState } from 'react';
-import {
-  HomeOutlined,ShoppingCartOutlined
-} from "@ant-design/icons";
-import { flushSync } from "react-dom";
-import getCart  from "./Apis/getCart";
+
+
 
 
 function Header(prop){
 
 return(
   <div className="appheader"> 
-  <div onClick={()=>prop.handelShow(false)}>Shopping Cart App</div>
-   <div onClick={()=>prop.handelShow(true)}>Cart
-  <sup>{prop.count}</sup>
-  </div>  
+  <div onClick={()=>prop.handelShow(false)} ><span style={{color:"white",fontWeight:'bold'}}>Shopping Cart App</span></div>
+   <div onClick={()=>prop.handelShow(true)}><span style={{color:"white",fontWeight:'bold'}}>Cart
+  <sup style={{fontWeight:"bold"}}> {prop.count}</sup>
+  </span></div>  
 
   </div>
 
 
 )
 }
-const AppCart=()=>{
-  const [open, setopen] = useState(false);
-  const [size, setSize] = useState();
 
-  const showDrawer = () => {
-  
-    setopen(true);
-    console.log("===",open)
-  };
-  const onClose = () => {
-   
-    setopen(false);
-  };
-return(
-  <div>
-<Badge   onClick={showDrawer} count={7} style={{cursor:"pointer", fontSize:'12px',marginRight:'40px'}}>
- <ShoppingCartOutlined  style={{ cursor:"pointer",fontSize:'32px',marginRight:'40px'}}></ShoppingCartOutlined>
- </Badge>
- <Drawer  title="Drawer" 
-//  visible={true}  
- closable={true}  onClose={onClose} open={open}
- >
-  <h1>hello</h1>
- </Drawer>
- </div>
-)
-}
 
 
 export default  Header
